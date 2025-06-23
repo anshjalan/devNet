8e6e68652510bd2486f9b8e10105758cfcc68b46
anshjalan <jalanansh25@gmail.com>
2025-06-24 05:10:21 +0530

    url changes

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const io = initializeSocket(server);
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://dev-net-six.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://dev-net-six.vercel.app'],
     credentials: true,
   })
 );
